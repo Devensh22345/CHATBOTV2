5674bf162327f1474710a0e065c3cd395dc7d354
Devensh22345 <kumardevensh0@gmail.com>
2025-02-21 01:51:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 API_ID = "29893020"
 API_HASH = "28e79037f0b334ef0503466c53f08af5"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING1 = getenv("STRING_SESSION", None)
+STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", 6399386263))
 SUPPORT_GRP = "Blissfull_Music"
