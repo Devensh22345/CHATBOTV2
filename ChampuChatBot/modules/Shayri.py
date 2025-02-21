2a20f9f5a1367f6d2c90326437749ca2ccad13a7
Devensh22345 <kumardevensh0@gmail.com>
2025-02-21 23:20:57 +0530

    Update Shayri.py

--- ChampuChatBot/modules/Shayri.py
+++ ChampuChatBot/modules/Shayri.py
@@ -71,10 +71,10 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "❍ 𝐒ᴜᴘᴘᴏʀᴛ ❍", url=f"https://t.me/akaChampu"
+                        "❍ 𝐒ᴜᴘᴘᴏʀᴛ ❍", url=f"https://t.me/{SUPPORT_GRP}"
                     ),
                     InlineKeyboardButton(
-                        "❍ 𝐔ᴘᴅᴀᴛᴇ ❍", url=f"https://t.me/ChampuBots"
+                        "❍ 𝐔ᴘᴅᴀᴛᴇ ❍", url=f"https://t.me/{SUPPORT_GRP}"
                     ),
                 ]
             ]
